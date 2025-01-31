3a9f38acdd92278cdce03bd2431409f6a0ac73f5
Taz <43081043+Tazhossain@users.noreply.github.com>
2025-02-01 00:27:14 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const settings = {
     monetag: "4e286c0f693c0db40ba5a1fbad04e921",
-    adminPassword: "YourSecurePasswordHere", // Change this
-    apiBaseUrl: "https://api.earnify.com"
+    adminPassword: "Admin123", // Change this
+    apiBaseUrl: "https://earnifybd.netlify.app"
 };
