aeb2e89f49e19797282bd8ac1cd03fe9bd0bf1f5
Taz <43081043+Tazhossain@users.noreply.github.com>
2025-02-01 00:50:34 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const settings = {
-    monetag: "4e286c0f693c0db40ba5a1fbad04e921",
-    adminPassword: "Admin123", // Change this
-    apiBaseUrl: "https://earnifybd.netlify.app"
+    apiBaseUrl: "https://earnifybd.netlify.app/.netlify/functions/api",
+    adminUser: "admin",
+    adminPass: "Admin123",
+    monetagID: "4e286c0f693c0db40ba5a1fbad04e921"
 };
